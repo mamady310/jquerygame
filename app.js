d447c4273156867a370b97db63d04ce6229495ec
Mamady Sillah <supurpisces310@msn.com>
2020-08-22 21:36:44 -0400

    created games.click/puase/sel for game play. did concat of boxes aray so that each color has a mtach for matching game

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const boxes = ['red', 'cyan', 'teal', 'purple', 'yellow', 'green', 'pink', 'gold']
 
-game = {};
+const game = {};
 $('.startBtn').click(startGame);
 
 function startGame(){
-    console.log('start');
+// console.log('start');
+$('.startBtn').hide();
+game.clicks = 0;
+game.puase = false;    
+game.sel =[];
+//concat boxes array so that each color has a match
+game.newArray = boxes.concat(boxes);
+console.log(game.newArray);
 }
 
