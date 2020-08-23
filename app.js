ab16f4cdc2df0acf603400d9bde9efbdbf7842f7
Mamady Sillah <supurpisces310@msn.com>
2020-08-22 21:40:52 -0400

    created function to loop through newArray and have key/value for each color

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ game.puase = false;
 game.sel =[];
 //concat boxes array so that each color has a match
 game.newArray = boxes.concat(boxes);
-console.log(game.newArray);
+// console.log(game.newArray);
+$('.game').html('');
+$.each(game.newArray, function(key, value){
+    console.log(key);
+    console.log(value)
+})
 }
 
