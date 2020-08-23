08023192f9d4efb50d59983ec1424a46005559f7
Mamady Sillah <supurpisces310@msn.com>
2020-08-22 21:24:28 -0400

    created startGame function in app.js. when start game is clicked, the number of clicks is displayed on console

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 const boxes = ['red', 'cyan', 'teal', 'purple', 'yellow', 'green', 'pink', 'gold']
+
+game = {};
+$('.startBtn').click(startGame);
+
+function startGame(){
+    console.log('start');
+}
+
