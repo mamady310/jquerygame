325a57bfa8b0a26da80f445dc01b64e57cf4daec
Mamady Sillah <supurpisces310@msn.com>
2020-08-24 21:22:23 -0400

    added functionality for game

--- app.js
+++ app.js
@@ -16,12 +16,20 @@ $('.game').html('');
 $.each(game.newArray, function(key, value){
     // console.log(key);
     // console.log(value)
-    let box = $('<div>')
+    let box = $('<div>');
     console.log(box);
     box.addClass('box');
     box.data('cnt', key + 1);
-    box.data('val', value)
-    let back =$('<div>')
+    box.data('val', value);
+    let back = $('<div>');
+    back.addClass('back');
+    back.html(key+1);
+    box.append(back);
+    let front = $('<div>');
+    front.css('background-color', value);
+    front.text(value);
+    front.addClass('front');
+    box.append(front);
 })
 }
 
