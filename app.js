c61208684e15792640f737d06a2a2370dc1e05b1
Mamady Sillah <supurpisces310@msn.com>
2020-08-24 22:32:00 -0400

    added arrayRandom function to randomize colors for game play

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const boxes = ['red', 'cyan', 'teal', 'purple', 'yellow', 'green', 'pink', 'gold
 const game = {};
 $('.startBtn').click(startGame);
 
+function arrayRandom(arr) {
+    arr.sort(function(){
+        return .5 - Math.random();
+    })
+}
+
 function startGame(){
-// console.log('start');
+console.log('start');
 $('.startBtn').hide();
 game.clicks = 0;
 game.puase = false;    
 game.sel =[];
 //concat boxes array so that each color has a match
 game.newArray = boxes.concat(boxes);
+//function to randomize colors
+arrayRandom(game.newArray);
 // console.log(game.newArray);
 $('.game').html('');
 $.each(game.newArray, function(key, value){
-    // console.log(key);
-    // console.log(value)
+    console.log(key);
+    console.log(value);
     let box = $('<div>');
     console.log(box);
     box.addClass('box');
-    box.data('cnt', key + 1);
+    box.data('cnt',key+1);
     box.data('val', value);
     let back = $('<div>');
     back.addClass('back');
@@ -30,6 +38,8 @@ $.each(game.newArray, function(key, value){
     front.text(value);
     front.addClass('front');
     box.append(front);
+    $('.game').append(box);
 })
 }
 
+ 
